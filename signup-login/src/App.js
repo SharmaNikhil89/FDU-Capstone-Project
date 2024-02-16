02517386d2a695c5d86aeb0f552abe17faecb9a9
SharmaNikhil89 <160225120+SharmaNikhil89@users.noreply.github.com>
2024-02-16 10:23:05 -0800

    Add files via upload

---
+++ signup-login/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SignupLogin from './Components/SignupLogin/SignupLogin';
+
+function App() {
+  return (
+    <div>
+      <SignupLogin/>
+    </div>
+  );
+}
+
+export default App;
